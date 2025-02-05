27a8a573f50bf3a8a4da0c6415317d52a0381e4d
YassonDC <yassonword@gmail.com>
2025-02-04 19:35:05 -0500

    create repository

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,16 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { 
+    files: ["**/*.{js,mjs,cjs,ts}"], 
+    rules: {
+      "eqeqeq": ["error", "always"],
+      "no-empty-function": "error",
+      "@typescript-eslint/no-duplicate-enum-values": "warn",
+      "@typescript-eslint/no-explicit-any": "warn"
+    },
+    languageOptions: { globals: globals.node },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
